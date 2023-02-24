84dbaf8092318d8aaa2581b12ea334a6e8256e29
DevShasa <shasa.thuo@gmail.com>
2023-02-24 22:05:00 +0300

    fixed next env variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,14 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: process.env.next_apiKey,
-	authDomain: process.env.next_authDomain,
-	projectId: process.env.next_projectId,
-	storageBucket: process.env.next_storageBucket,
-	messagingSenderId: process.env.next_messagingSenderId,
-	appId: process.env.next_appId,
+	apiKey: process.env.NEXT_PUBLIC_apiKey,
+	authDomain: process.env.NEXT_PUBLIC_authDomain,
+	projectId: process.env.NEXT_PUBLIC_projectId,
+	storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+	messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+	appId: process.env.NEXT_PUBLIC_appId
 };
-
 
 // Initialize Firebase for server side rendering
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
