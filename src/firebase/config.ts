9adfcbb9182da1df1456c186e07b80bd95c8748e
DevShasa <shasa.thuo@gmail.com>
2023-04-04 21:00:47 +0300

    changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,6 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// NEXTJS env must start with NEXT_PUBLIC or they wont work 
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_apiKey,
 	authDomain: process.env.NEXT_PUBLIC_authDomain,
